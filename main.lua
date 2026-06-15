fadb462239fb92052029f72672943050aaf9e939
amrho94 <landonsxbox222@gmail.com>
2026-06-15 13:22:53 -0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,24 +2,59 @@ repeat task.wait() until game:IsLoaded()
 if shared.vape then shared.vape:Uninject() end
 
 local vape
-local loadstring = function(...)
-	local res, err = loadstring(...)
-	if err and vape then
-		vape:CreateNotification('Vape', 'Failed to load : '..err, 30, 'alert')
+
+local real_loadstring = loadstring
+local function compileChunk(source, chunkName)
+	local fn, err = real_loadstring(source, chunkName)
+	if not fn then
+		error(('Failed to compile %s:\n%s'):format(tostring(chunkName), tostring(err)))
 	end
-	return res
+	return fn
 end
+
+local function runChunk(source, chunkName, ...)
+	local fn = compileChunk(source, chunkName)
+	local ok, result = pcall(fn, ...)
+	if not ok then
+		error(('Failed to run %s:\n%s'):format(tostring(chunkName), tostring(result)))
+	end
+	return result
+end
+
 local queue_on_teleport = queue_on_teleport or function() end
+
 local isfile = isfile or function(file)
 	local suc, res = pcall(function()
 		return readfile(file)
 	end)
 	return suc and res ~= nil and res ~= ''
 end
+
+local isfolder = isfolder or function(folder)
+	if listfiles then
+		local suc = pcall(function()
+			listfiles(folder)
+		end)
+		return suc
+	end
+	return false
+end
+
+local makefolder = makefolder or function(folder)
+	error('Executor is missing makefolder. Cannot create folder: '..tostring(folder))
+end
+
 local cloneref = cloneref or function(obj)
 	return obj
 end
+
 local playersService = cloneref(game:GetService('Players'))
+
+local function ensureFolder(path)
+	if not isfolder(path) then
+		makefolder(path)
+	end
+end
 
 local function downloadFile(path, func)
 	if not isfile(path) then
@@ -40,6 +75,7 @@ end
 local function finishLoading()
 	vape.Init = nil
 	vape:Load()
+
 	task.spawn(function()
 		repeat
 			vape:Save()
@@ -59,12 +95,14 @@ local function finishLoading()
 					loadstring(game:HttpGet('https://raw.githubusercontent.com/amrho94/Vape/'..readfile('vape/profiles/commit.txt')..'/loader.lua', true), 'loader')()
 				end
 			]]
+
 			if shared.VapeDeveloper then
 				teleportScript = 'shared.VapeDeveloper = true\n'..teleportScript
 			end
 			if shared.VapeCustomProfile then
 				teleportScript = 'shared.VapeCustomProfile = "'..shared.VapeCustomProfile..'"\n'..teleportScript
 			end
+
 			vape:Save()
 			queue_on_teleport(teleportScript)
 		end
@@ -73,36 +111,54 @@ local function finishLoading()
 	if not shared.vapereload then
 		if not vape.Categories then return end
 		if vape.Categories.Main.Options['GUI bind indicator'].Enabled then
-			vape:CreateNotification('Finished Loading', vape.VapeButton and 'Press the button in the top right to open GUI' or 'Press '..table.concat(vape.Keybind, ' + '):upper()..' to open GUI', 5)
+			vape:CreateNotification(
+				'Finished Loading',
+				vape.VapeButton and 'Press the button in the top right to open GUI'
+					or 'Press '..table.concat(vape.Keybind, ' + '):upper()..' to open GUI',
+				5
+			)
 		end
 	end
 end
 
+ensureFolder('vape')
+ensureFolder('vape/profiles')
+ensureFolder('vape/assets')
+
 if not isfile('vape/profiles/gui.txt') then
 	writefile('vape/profiles/gui.txt', 'new')
 end
+
 local gui = readfile('vape/profiles/gui.txt')
 
-if not isfolder('vape/assets/'..gui) then
-	makefolder('vape/assets/'..gui)
-end
-vape = loadstring(downloadFile('vape/guis/'..gui..'.lua'), 'gui')()
+ensureFolder('vape/assets/'..gui)
+
+-- IMPORTANT:
+-- The old version did this:
+-- vape = loadstring(downloadFile('vape/guis/'..gui..'.lua'), 'gui')()
+-- If the GUI has a syntax error, loadstring returns nil, then nil() crashes at line 90.
+-- This version shows the REAL GUI error instead.
+local guiSource = downloadFile('vape/guis/'..gui..'.lua')
+vape = runChunk(guiSource, 'gui')
 shared.vape = vape
 
 if not shared.VapeIndependent then
-	loadstring(downloadFile('vape/games/universal.lua'), 'universal')()
+	runChunk(downloadFile('vape/games/universal.lua'), 'universal')
+
 	if isfile('vape/games/'..game.PlaceId..'.lua') then
-		loadstring(readfile('vape/games/'..game.PlaceId..'.lua'), tostring(game.PlaceId))(...)
+		runChunk(readfile('vape/games/'..game.PlaceId..'.lua'), tostring(game.PlaceId), ...)
 	else
 		if not shared.VapeDeveloper then
 			local suc, res = pcall(function()
 				return game:HttpGet('https://raw.githubusercontent.com/amrho94/Vape/'..readfile('vape/profiles/commit.txt')..'/games/'..game.PlaceId..'.lua', true)
 			end)
 			if suc and res ~= '404: Not Found' then
-				loadstring(downloadFile('vape/games/'..game.PlaceId..'.lua'), tostring(game.PlaceId))(...)
+				downloadFile('vape/games/'..game.PlaceId..'.lua')
+				runChunk(readfile('vape/games/'..game.PlaceId..'.lua'), tostring(game.PlaceId), ...)
 			end
 		end
 	end
+
 	finishLoading()
 else
 	vape.Init = finishLoading
